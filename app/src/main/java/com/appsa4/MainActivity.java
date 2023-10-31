12fd60b3370b0d825f70aa618b1564bcfa7e2a66
Abidluayraihantaufik <spaskeren30@gmail.com>
2023-10-31 07:34:48 +0700

    Initial commit

---
+++ app/src/main/java/com/appsa4/MainActivity.java
@@ -0,0 +1,35 @@
+package com.appsa4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private int mCount= 0;
+    private TextView mShowCount;
+    private int secondCount = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitytoast);
+        mShowCount= (TextView) findViewById(R.id.show_count);
+    }
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        int next = mCount;
+        mCount = secondCount;
+        secondCount = next + mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(next));
+    }
+}
---
+++ app/src/main/java/com/appsa4/ScrollingIcecold.java
@@ -0,0 +1,13 @@
+package com.appsa4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScrollingIcecold extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_scrollicecold);
+    }
+}
